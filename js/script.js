5e231e785884c4cb3e3e5e215b4859a67b0262ba
nicolozibra1 <nico.zibra@gmail.com>
2023-03-07 17:16:04 +0100

    fix-syntax-js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let firstName = prompt (`Qual\'è il tuo nome?`);
-let lastName = prompt (`Qual\'è il tuo cognome?`);
-let favColor = prompt (`Qual\'è il tuo colore preferito?`);
+let firstName = prompt("Qual\'è il tuo nome?");
+let lastName = prompt("Qual\'è il tuo cognome?");
+let favColor = prompt("Qual\'è il tuo colore preferito?");
 let notValid = "Inserisci correttamente i tuoi dati."
 
 const password = firstName + lastName + favColor + 21; 
